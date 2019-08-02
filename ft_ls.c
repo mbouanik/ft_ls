695443e605297df0fc0c70f3a87c121a5c808c03
Matthis Bouanik <mbouanik@student.42.us.org>
2019-08-02 14:40:11 -0700

    using stat

--- ft_ls.c
+++ ft_ls.c
@@ -6,16 +6,16 @@
 /*   By: mbouanik <mbouanik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 14:57:36 by mbouanik          #+#    #+#             */
-/*   Updated: 2019/08/02 13:55:30 by mbouanik         ###   ########.fr       */
+/*   Updated: 2019/08/02 14:38:37 by mbouanik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
   #include <dirent.h>
   #include <stdio.h>
   #include "ft_printf/ft_printf.h"
- #include <sys/stat.h>
+  #include <sys/stat.h>
 
-void op_dir(struct dirent * dp){
+/*void op_dir(struct dirent * dp){
   DIR * dir;
  struct dirent *ds;
   if (dp->d_type == 4 ){
@@ -30,7 +30,7 @@ void op_dir(struct dirent * dp){
       }
   }
 }
-
+*/
 int ft_ls(void)
 {
     DIR * dir;
@@ -46,7 +46,7 @@ int ft_ls(void)
        while ((dp = readdir(dir)) != NULL) {
           ft_printf("%s ", dp->d_name);
 		  stat(dp->d_name, buf);
-		  ft_printf(" stat: %x\n", buf->st_mode);
+		  ft_printf(" stat: %o\n", buf->st_mode);
          //  op_dir(dp);
       }
       closedir(dir);
@@ -57,4 +57,4 @@ int main(int argc, char const *argv[])
 {
   ft_ls();
   return 0;
-}
+}	
