b25a9076248d616d23b3abd36b056d89dbd6e2d4
Mathis <mbouanik@student.42.us.org>
2019-08-27 11:21:27 -0700

    fixed ls

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: mbouanik <mbouanik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 14:57:36 by mbouanik          #+#    #+#             */
-/*   Updated: 2019/08/26 22:47:11 by mbouanik         ###   ########.fr       */
+/*   Updated: 2019/08/27 11:19:46 by mbouanik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
  #include <dirent.h>
   #include <stdio.h>
   #include "ft_printf/ft_printf.h"
-//   #include "libft/libft.h"
   #include <sys/stat.h>
   #include <sys/types.h>
   #include <pwd.h>
@@ -23,21 +22,12 @@
   #include <sys/ioctl.h>
 
 
-
-
 typedef struct	s_dir_name 
 {
 	char *name;
-	// char * path;
 	struct s_dir_name* next;
 
 }				t_dir_name;
-
-
-
-
-
-
 
 
 void		free_subdir(t_dir_name ** sub_dir){
@@ -55,8 +45,6 @@ t_dir_name * 	 ft_new_subdir(char *s){
 	new_dir = NULL;
 	if((new_dir = (t_dir_name *)malloc(sizeof(t_dir_name)))){
 		new_dir->name = s;
-		// ft_strjoin("./", s);
-		// new_dir->path = NULL;
 		new_dir->next = NULL;
 	}
 	return (new_dir);
@@ -80,65 +68,48 @@ void			ft_add_subdir(t_dir_name** nsub_dir ,char * str){
 void 	display_subdir(struct dirent * dp, char* name, struct stat *buf, t_dir_name  *sub_dir){
 
 	DIR * dir = opendir(name);
-	// struct stat *buf;
-	// bzero(buf, sizeof(struct stat));
 	while ((dp = readdir(dir)) != NULL) {
 		stat(ft_strjoin(name, dp->d_name), buf);
-		if (dp->d_name[0] != '.')
-			ft_printf("sub: %s -- %X ",dp->d_name, buf->st_mode & S_IFMT);
+		// if (dp->d_name[0] != '.')
+			// ft_printf("sub: %s -- %X ",dp->d_name, buf->st_mode & S_IFMT);
 		if (dp->d_name[0] != '.'){
 			if ((buf->st_mode & S_IFMT) == S_IFDIR)
 				ft_add_subdir(&sub_dir, ft_strjoin(name, dp->d_name));
-				ft_printf("---%s ---", ft_strjoin(name, dp->d_name));
-			ft_printf("%s:\n", dp->d_name);
+				// ft_printf("---%s ---", ft_strjoin(name, dp->d_name));
+			ft_printf("%s  ", dp->d_name);
 		}
 	}
-		// bzero(buf, sizeof(struct stat));
-
 	ft_printf("\n");
 	if (sub_dir){
-		//  bzero(buf, sizeof(struct stat));
-	ft_printf("\n%s:\n", sub_dir->name);
-		// if (buf->st_mode & S_IFDIR)
-					display_subdir(dp, ft_strjoin(sub_dir->name, "/"), buf, sub_dir->next);
+		ft_printf("\n%s:\n", sub_dir->name);
+		display_subdir(dp, ft_strjoin(sub_dir->name, "/"), buf, sub_dir->next);
 		free_subdir(&sub_dir);
-	// 	// sub_dir = sub_dir->next;
-	// 	free_subdir(&sub_dir);
-	// closedir(dir);
 	}
-
 }
 
 
 void 			ft_display_dir(struct dirent * dp, DIR * dir, struct stat * buf){
 	
 	t_dir_name  *sub_dir;
-	// t_tree_dir_name * sub_dir;
+
 	sub_dir = NULL;
 	
 	while ((dp = readdir(dir)) != NULL) {
 		stat(dp->d_name, buf);
-		if (dp->d_name[0] != '.')
-			ft_printf("\nsub: %s -- %X ",dp->d_name, buf->st_mode & S_IFMT);
+		// if (dp->d_name[0] != '.')
+			// ft_printf("\nsub: %s -- %X ",dp->d_name, buf->st_mode & S_IFMT);
 
 		if (dp->d_name[0] != '.'){
 			if ((buf->st_mode & S_IFMT) == S_IFDIR)
-				ft_add_subdir(&sub_dir, ft_strjoin("./", dp->d_name));
-			ft_printf("%s\n", dp->d_name);
+				ft_add_subdir(&sub_dir, ft_strjoin("/", dp->d_name));
+			ft_printf("%s  ", dp->d_name);
 		}
-		// bzero(buf, sizeof(struct stat));
 	}
 	if (sub_dir){
 		ft_printf("\n%s:\n", ft_strjoin(sub_dir->name, "/"));
-		// ft_printf("\n./%s:\n", sub_dir->name);
-		// ft_printf("\n Before %X: \n", buf->st_mode);
-		// bzero(buf, sizeof(struct stat));
-		// ft_printf("\n After :%X:\n", buf->st_mode);
 		display_subdir(dp, ft_strjoin(sub_dir->name, "/"), buf, sub_dir->next);
 		free_subdir(&sub_dir);
 	}
-		if (sub_dir)
-			ft_printf("BLoooeo");
 }
 
 
@@ -149,7 +120,7 @@ int ft_ls(void)
 	dir = NULL;
 	struct stat buf;
 	
-	dir = opendir(".");
+	dir = opendir("/");
 
 	if(dir == NULL){
 		perror("error");
