eaa9d1a4182df89a4f3f15a203b8853f07a320b6
Mathis <mbouanik@student.42.us.org>
2019-10-01 16:30:03 -0700

    normed display dir

--- ft_display_sub_dir.c
+++ ft_display_sub_dir.c
@@ -6,7 +6,7 @@
 /*   By: mbouanik <mbouanik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 18:53:41 by mathis            #+#    #+#             */
-/*   Updated: 2019/10/01 15:27:16 by mbouanik         ###   ########.fr       */
+/*   Updated: 2019/10/01 16:28:41 by mbouanik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,45 +28,20 @@ void	display_subdir(struct dirent *dp, char *name, struct stat buf, t_dir_name *
 	{
 		while ((dp = readdir(dir)) != NULL)
 		{
-			path = ft_strjoin(name, dp->d_name);
-			lstat(path, &buf);
-			if ((g_flags) & 2)
-				ft_add_subdir(&st_dir, ft_strdup(dp->d_name), buf);
-			else
-			{
-				if (dp->d_name[0] != '.')
-					ft_add_subdir(&st_dir, ft_strdup(dp->d_name), buf);
-			}
-			if (((buf.st_mode & S_IFMT) == S_IFDIR))
-			{
-				if (dp->d_name[0] == '.' && ft_hidden_dir(dp->d_name)
-				&& (g_flags & 2))
-					ft_add_subdir(&sub_dir, ft_strdup(path), buf);
-				else if (dp->d_name[0] != '.')
-					ft_add_subdir(&sub_dir, ft_strdup(path), buf);
-			}
+			g_path = ft_strjoin(name, dp->d_name);
+			lstat(g_path, &buf);
+			ft_set(dp, buf, &st_dir, &sub_dir);
 			bzero(&buf, sizeof(struct stat));
-			free(path);
+			free(g_path);
 		}
 	}
 	if (!dp && dir)
 		(void)closedir(dir);
-	if (g_flags & 16)
-		ft_printf("total %d\n", g_block);
-	while (st_dir)
-	{
-		if (g_flags & 16)
-			ft_printf("%s %d %s %s %6d %.15s %s%s\n",
-			st_dir->mode, st_dir->n_link, st_dir->pw_name, st_dir->gr_name,
-			st_dir->size, st_dir->date, st_dir->name, st_dir->linkname);
-		else
-			ft_printf("%-10s ", st_dir->name);
-		free_subdir(&st_dir);
-	}
-	g_block = 0;
-	if (!(g_flags & 16))
-		ft_printf("\n");
-	if (sub_dir && (g_flags & 1))
+	if ((g_flags & 16))
+		ft_display_flag_l(&st_dir);
+	else
+		ft_display_ls(&st_dir);
+	if (sub_dir)
 	{
 		path = ft_strjoin(sub_dir->name, "/");
 		ft_printf("\n%s:\n", sub_dir->name);
--- ft_ls.h
+++ ft_ls.h
@@ -15,9 +15,6 @@
 #include <grp.h>
 #include <uuid/uuid.h>
 
-
-
-
 typedef struct			s_dir_name 
 {
 	char 				*name;
@@ -64,7 +61,7 @@ char					*ft_strdup(const char *s1);
 char					*ft_strnew(size_t size);
 char					*ft_strjoin(char const *s1, char const *s2);
 char					*ft_strsub(char const *s, unsigned int start, size_t len);
-void					ft_display_rec(t_dir_name **sub_dir, struct stat buf, struct dirent *dp, char **file_name);
+void					ft_display_rec(t_dir_name **sub_dir, struct stat buf, struct dirent *dp, char **name);
 void					ft_set(struct dirent *dp, struct stat buf, t_dir_name **st_dir, t_dir_name **sub_dir);
 void					ft_display_flag_l(t_dir_name **st_dir);
 void					ft_display_ls(t_dir_name **st_dir);
